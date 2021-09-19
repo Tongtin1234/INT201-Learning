a23df337ab0b238fbf1f00348d4f437c6b127c3f
Tongtin1234 <73164501+Tongtin1234@users.noreply.github.com>
2021-09-19 16:17:47 +0700

    Add files via upload

---
+++ project01/app.js
@@ -0,0 +1,24 @@
+/*var x =20
+{
+    var x =100
+    let y = 15
+}*/
+//console.log(x)
+//คำสั่ง var จะสามารถเรียกใช้งานได้ทั้งหมด แต่ let จะเรียกใช้งานได้แค่ใน{}เท่านั้น
+
+//let lotto_number = prompt("กรอกเลขที่ต้องการซื้อ : ")
+//document.getElementById("lotto_num").innerHTML=lotto_number
+//document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+console.log(1==1)
+console.log(2==2)
+console.log(3==3)
+console.log(4=="4") 
+console.log(5==6)
+
+console.log("6" === "6") //เช็คข้อมูลประเภทเดียวกัน
+
+console.log("4" !== 4)
+console.log("hello" != "Hello")
+console.log("4" != "4")
+console.log(10>5)
+console.log(9>=9)
